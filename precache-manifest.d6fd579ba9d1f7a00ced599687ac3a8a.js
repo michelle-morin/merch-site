9890b4c7c10ebef333234551ad3a9d7ff1c3fa75
Michelle Morin <morin2m@gmail.com>
2020-04-26 10:52:25 -0700

    Updates

---
+++ precache-manifest.d6fd579ba9d1f7a00ced599687ac3a8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedf6b68d532fcd86261fd660aaab3d3",
+    "url": "/merch-site/index.html"
+  },
+  {
+    "revision": "7c3ca40b42d07bd5f1a3",
+    "url": "/merch-site/static/css/main.955562a4.chunk.css"
+  },
+  {
+    "revision": "90631e0b5b4ab80e838d",
+    "url": "/merch-site/static/js/2.950cf9f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/merch-site/static/js/2.950cf9f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3ca40b42d07bd5f1a3",
+    "url": "/merch-site/static/js/main.e15f8929.chunk.js"
+  },
+  {
+    "revision": "086b0a2972bc407ea033",
+    "url": "/merch-site/static/js/runtime-main.628cee91.js"
+  }
+]);
